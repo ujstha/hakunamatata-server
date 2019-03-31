5c7ba9ae9402c0408df2a0d5a4bceaf37cc6b3e2
ujstha <ujstha27@gmail.com>
2019-03-31 14:43:17 +0300

    image change in user

--- controllers/posts.js
+++ controllers/posts.js
@@ -131,6 +131,34 @@ module.exports = {
     }
   },
 
+  async UploadImage(req, res) {
+    cloudinary.uploader.upload(req.body.image, async result => {
+      await User.update(
+        {
+          _id: req.user._id
+        },
+        {
+          $push: {
+            images: {
+              imgId: result.public_id,
+              imgVersion: result.version
+            }
+          }
+        }
+      )
+        .then(() =>
+          res
+            .status(HttpStatus.OK)
+            .json({ message: 'Image uploaded successfully' })
+        )
+        .catch(err =>
+          res
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .json({ message: 'Error uploading image' })
+        );
+    });
+  },
+
   async GetAllPosts(req, res) {
     try {
       const today = moment().startOf('day');
